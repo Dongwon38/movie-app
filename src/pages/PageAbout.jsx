6495e1b41b92e027263a5f2d62e76cc058e8fe0a
Laura Kochen <lmccartney3@my.bcit.ca>
2024-08-04 14:12:52 -0700

    added text to about section

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function PageAbout() {
   return (
     <main>
-      <h1>Page About</h1>
+
+      <h1>Welcome to MOVIEPIN</h1>
+      <p>Hey there, welcome to MOVIEPIN! We're excited to help you discover movies that match your unique tastes.</p>
+      <h2>Discover and Save Favourites</h2>
+      <p>Our smart recommendation system suggests movies you'll love, saving you from endless scrolling. Plus, you can save your favourites to create a personal movie library.</p>
+      <p>With MOVIEPIN, sharing your favourite movie lists with friends and family is a breeze, making movie nights even more enjoyable.</p>
+      <h2>Enjoy your movie journey with MOVIEPIN!</h2>
     </main>
   );
 }
