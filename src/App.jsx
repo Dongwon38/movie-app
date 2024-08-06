a19ee44a6b068f53f31513b5eba3cd11f2d905aa
Benjamin <benjamin.4179@gmail.com>
2024-08-06 14:02:59 -0700

    Added intro animation to page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import AppRouter from "./routers/AppRouter";
 import "./styles/styles.css";
 
+//For Animation Page
+import IntroPage from "./pages/IntroPage";
+import SvgAnimation from "./pages/SvgAnimation";
+
 function App() {
   return (
     <>
+      <div className="end-animation">
+      <SvgAnimation/>
+      <IntroPage/>
+      </div>
+      <div className="movie-mock-up-class">
       <AppRouter />
+      </div>
     </>
   );
 }
---
+++ src/pages/IntroPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { gsap } from "gsap";
+import { useLayoutEffect, useRef } from 'react';
+
+
+function IntroPage() {
+
+  
+}
+
+export default IntroPage;
