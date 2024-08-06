7dae6fc3f51dd49f6c6ba610d963803517cbf5ad
Dongwon Kang <dongwon.dwk@gmail.com>
2024-08-06 14:04:59 -0700

    moved all images to public folder, added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import logo from "../../public/assets/images/logo/logo3.png";
 
 function Header() {
   return (
     <header>
-      <img src="../logo3.png" alt="website logo Movie-Pin" />
+      <a href="/">
+        <img src={logo} alt="website logo Movie-Pin" />
+      </a>
     </header>
   );
 }
