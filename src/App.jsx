55c9da7b7823029ab455f586533f88a27c6db2df
Dongwon Kang <dongwon.dwk@gmail.com>
2024-08-06 14:15:40 -0700

    optimized on the intro animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,17 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 
 //For Animation Page
-import IntroPage from "./pages/IntroPage";
 import SvgAnimation from "./pages/SvgAnimation";
 
 function App() {
   return (
     <>
-      <div className="end-animation">
-        <SvgAnimation/>
-        <IntroPage/>
-      </div>
-      <div className="movie-mock-up-class">
-        <Provider store={store}>
-          <AppRouter />
-        </Provider>
-      </div>
+      {/* <div className="end-animation">
+        <SvgAnimation />
+      </div> */}
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
     </>
   );
 }
