2f91a8ccf7d88e1b4eb09f4d0a9e36fa007044f1
Laura Kochen <lmccartney3@my.bcit.ca>
2024-08-21 13:22:36 -0700

    added about image

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import background from '/assets/images/bg/aboutPg-m.png';
 
 function PageAbout() {
   return (
-    <main>
-      <h1>Page About</h1>
-    </main>
+<main className="about-page">
+
+<h1 className="about-title">Welcome to MOVIEPIN</h1>
+<img src={background} alt="" />
+      <p>Hey there, welcome to MOVIEPIN! We're excited to help you discover movies that match your unique tastes.</p>
+      <h2>Discover and Save Favourites</h2>
+      <p>Our smart recommendation system suggests movies you'll love, saving you from endless scrolling. Plus, you can save your favourites to create a personal movie library.</p>
+      <p>With MOVIEPIN, sharing your favourite movie lists with friends and family is a breeze, making movie nights even more enjoyable.</p>
+      <h2>Enjoy your movie journey with MOVIEPIN!</h2>
+</main>
   );
 }
 
 export default PageAbout;
+
+
