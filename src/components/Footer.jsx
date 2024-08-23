6d8884b07fd4d6cd308f4752b247448a4b7d6912
JM Hore <kira@MacBook-Air.local>
2024-08-23 11:38:57 -0700

    added footer and styles on the header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import logo from "../../public/assets/images/logo/logo3.png";
 
 function Footer() {
-  return <footer>footer</footer>;
+  return <footer
+            className="footer-container"
+            >
+            <img  className="footer-logo"
+                  src={logo} 
+                  alt="Movie Pin Logo" />
+          </footer>;
 }
 
 export default Footer;
