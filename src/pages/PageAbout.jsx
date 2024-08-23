2548386c8e5031d5a5a585f94513f3c982897ebf
Laura Kochen <lmccartney3@my.bcit.ca>
2024-08-23 09:50:26 -0700

    added bacckground to about page text block

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -10,19 +10,22 @@ function PageAbout() {
 <main>
 <img className = "about-image" src={background} alt="Background" />
 
-  <container className="container-about">
-<h1 className="about-title">Welcome to MOVIEPIN</h1>
+
+
+
+<section className="popcorn-image">
+  <div className="container-about">
+      <h1 className="about-title">Welcome to MOVIEPIN</h1>
       <p>Hey there, welcome to MOVIEPIN! We're excited to help you discover movies that match your unique tastes.</p>
 
       <h2>Discover and Save Favourites</h2>
       <p>Our smart recommendation system suggests movies you'll love, saving you from endless scrolling. Plus, you can save your favourites to create a personal movie library.</p>
       <p>With MOVIEPIN, sharing your favourite movie lists with friends and family is a breeze, making movie nights even more enjoyable.</p>
       <h2>Enjoy your movie journey with MOVIEPIN!</h2>
-      </container>
+  </div>
 
-      <container className="popcorn-image">
       <img className = "popcorn" src={popcorn} alt="Popcorn Background" />
-      </container>
+</section>
 
 </main>
   );
