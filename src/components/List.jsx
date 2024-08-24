dc956607aab88455c7870dca7e577219133bdfae
JM Hore <kira@MacBook-Air.local>
2024-08-24 16:05:39 -0700

    added hover overlay

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   endPoint,
   searchEndPoint,
@@ -10,7 +11,9 @@ import { Link } from "react-router-dom";
 import FavButton from "./FavButton";
 import PageButton from "./PageButton";
 import { GlobalContext } from "../context/GlobalState";
-import HeroSection from "./HeroSection";
+import pinUnfill from "../../public/assets/images/icons/pin-unfill.svg";
+import pinFill from "../../public/assets/images/icons/pin-fill.svg";
+import { addFav, deleteFav } from "../features/favs/favsSlice";
 
 function List({ category, page, setPage }) {
   // list to store data from API
@@ -21,11 +24,17 @@ function List({ category, page, setPage }) {
   // get GlobalContext
   const { searchText, countMovies } = useContext(GlobalContext);
 
+  // Access favorite movies from Redux store
+  const favorites = useSelector((state) => state.favs.items);
+  const dispatch = useDispatch();
+
   // Check if there is a search text
   const fetchUrl =
-    searchText == null || searchText == ""
+    searchText == null || searchText === ""
       ? `${endPoint}${category}?language=en-US&page=${page}`
       : `${searchEndPoint}?query=${searchText}&page=${page}`;
+  
+  const [hoveredMovieId, setHoveredMovieId] = useState(null);
 
   // get movie Data From API
   useEffect(() => {
@@ -49,18 +58,56 @@ function List({ category, page, setPage }) {
     // check change on "page" and "category"
   }, [page, category, searchText]);
 
+  const cutText = (text, maxLength) => {
+    if (text.length <= maxLength) return text;
+    return text.slice(0, maxLength) + '...';
+  };
+
+  const isFavorite = (id) => {
+    return favorites.some((fav) => fav.id === id);
+  };
+
+  const handleFavoriteToggle = (movie) => {
+    if (isFavorite(movie.id)) {
+      dispatch(deleteFav(movie));
+    } else {
+      dispatch(addFav(movie));
+    }
+  };
+
   return (
     <section>
       <div className="section-list">
       {movieList.map((movie) => {
+        const isFav = isFavorite(movie.id);
         return (
-          <article key={movie.id} className="movie-item">
+          <article
+            key={movie.id}
+            className="movie-item"
+            onMouseOver={() => setHoveredMovieId(movie.id)}
+            onMouseOut={() => setHoveredMovieId(null)}
+          >
             <Link to={`/detail/${movie.id}`}>
             <img
               src={`${poster_base_url}/${poster_size[3]}/${movie.poster_path}`}
               alt={movie.title}
               />
             </Link>
+              {hoveredMovieId === movie.id && (
+                <div className="overlay">
+                  <img  
+                    src={isFav ? pinFill : pinUnfill} 
+                    alt={isFav ? "Favorited" : "Not Favorited"}
+                    className="favPin"
+                    onClick={() => handleFavoriteToggle(movie)}
+                  />
+                  <h3>{movie.title}</h3>
+                  <p>{cutText(movie.overview, 100)}</p>
+                  <Link to={`/detail/${movie.id}`}>
+                  <p className="moreInfo">More Info</p>
+                  </Link>
+                </div>
+              )}
             <div className="list-links">
               <Link to={`/detail/${movie.id}`}>
                 <p>More Info</p>
@@ -77,3 +124,4 @@ function List({ category, page, setPage }) {
 }
 
 export default List;
+
